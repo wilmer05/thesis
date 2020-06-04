32969b87e3f0fa036d9e6e9c7b21a86dcf32d049
wilmer05 <wilmer0593@gmail.com>
2020-06-04 18:30:34 +0200

    adding experiment

---
+++ src/experiment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include"alg.hpp"
+
+using namespace std;
+
+double experiment(int n_items, int n_experiments) {
+    double mean = 0.0;
+
+
+    return mean;
+}
