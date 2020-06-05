805fecf62dfa0407933e3344e00b98c2d9834e88
wilmer05 <wilmer0593@gmail.com>
2020-06-05 18:09:39 +0200

    finishing experiments

--- src/experiment.cpp
+++ src/experiment.cpp
@@ -6,18 +6,26 @@
 
 using namespace std;
 
-double experiment(int n_items, int mode, int n_experiments) {
+double experiment(int n_items, int mode, int n_rounds) {
     double mean = 0.0;
+    if (n_rounds == 0)
+        return .0;
 
 
-    vector<double> weights = generate_input(n_items, mode);
-    vector<double> profits = generate_input(n_items, mode);
-    vector<candidate> r = nemhauser_ullman(weights, profits);
+    for(int i = 0; i < n_rounds; i++) {
+        vector<double> weights = generate_input(n_items, mode);
+        vector<double> profits = generate_input(n_items, mode);
+        vector<candidate> r = nemhauser_ullman(weights, profits);
 
-    cout << "Solutions:\n" << endl;
-    for(int i =0 ; i < r.size(); i++) {
-        cout << "\t" << r[i].weight << " " << r[i].profit << endl;
+        mean += (double) r.size();
     }
+
+    //cout << "Solutions:\n" << endl;
+    /* for(int i =0 ; i < r.size(); i++) {
+        cout << "\t" << r[i].weight << " " << r[i].profit << endl;
+    }*/
+
+    mean /= (double) n_rounds; 
 
     return mean;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,29 @@
 
 using namespace std;
 
+void run_experiments(int mode, int n_rounds, int n_experiments) {
+
+    cout << "Starting experiments\nN\t|\tNumber of pareto optimal solutions\n";
+    for(int i=1 ; i < n_experiments + 1; i++) {
+        double n_pareto_optimal_solutions = 
+                    experiment(i, mode, n_rounds);
+
+        cout << i << "\t|\t " << n_pareto_optimal_solutions << endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
 
-    if(argc < 3) {
-        cout << "Usage: \n\t./run <n_inputs> <mode>\n\n\t" << 
+    if(argc < 4) {
+        cout << "Usage: \n\t./run <mode> <n_rounds> <n_experiments>\n\n\t" << 
                 "modes\n\t\t1 : uniform distribution in [0,1]" <<
                 "\n\t\t2 : uniform distribution in [-1,1]" << endl;
         return 1;
     }
 
-    int n = atoi(argv[1]);
-    int mode = atoi(argv[2]);
-
-    experiment(n, mode, 1);
+    int mode = atoi(argv[1]);
+    int n_rounds = atoi(argv[2]);
+    int n_experiments = atoi(argv[3]);
+    run_experiments(mode, n_rounds, n_experiments);
     return 0;
 }
